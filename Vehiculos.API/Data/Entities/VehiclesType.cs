1deeeb683e06f9f2bfc0b65bd72998d67c373796
JHONATAN ALEXANDER MONSALVE RENDON <jhonatanmonsalve130089@correo.itm.edu.co>
2021-08-26 21:02:47 -0400

    add vehicle type

---
+++ Vehiculos.API/Data/Entities/VehiclesType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehiculos.API.Data.Entities
+{
+    public class VehiclesType
+    {
+        public int id { get; set; }
+
+        [Display(Name ="Tipo de Veichulo")]
+        [MaxLength(50, ErrorMessage ="El campo {0} no puede tener mas de {1} carácteres.")]
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        public string Description { get; set; }
+    }
+}
